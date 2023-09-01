57d9630bbed1d1d52640bda7767a22c28caadc29
urienb <73485409+urienb@users.noreply.github.com>
2023-09-01 20:57:51 +0200

    Update __init__.py

--- underground/models/__init__.py
+++ underground/models/__init__.py
@@ -1,4 +1,4 @@
 from underground.models.autoencoder_net import Autoencoder
 
-def get_model():
-    model = Autoencoder()
+def get_model(in_channels=1, downsampling_factors=[2,2,2], fmaps=32, fmul=2, kernel_size = 3):
+    model = Autoencoder(in_channels, downsampling_factors, fmaps, fmul, kernel_size)
