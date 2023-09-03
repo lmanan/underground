6151d3af45e36ddf0107ef0ec6e86c5280f938eb
Manan <manan.lalit@gmail.com>
2023-09-03 14:02:30 +0000

    Delete old files

--- underground/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from underground.models.autoencoder_net import Autoencoder
-
-def get_model(in_channels=1, downsampling_factors=[2,2,2], fmaps=32, fmul=2, kernel_size = 3):
-    model = Autoencoder(in_channels, downsampling_factors, fmaps, fmul, kernel_size)
